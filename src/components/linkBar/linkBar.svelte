ce4508c741574c3e6960d7c62412b2f967035226
Rurucchi <rurucchi.pro@gmail.com>
2023-04-09 12:54:42 +0200

    deployment fix

--- src/components/linkBar/linkBar.svelte
+++ src/components/linkBar/linkBar.svelte
@@ -1,29 +1,29 @@
 <script>
-	export let ghLink;
-	export let liLink;
+  export let ghLink;
+  export let liLink;
 
-	import styles from './styles.module.css';
+  import styles from "./styles.module.css";
 
-	function handleClick() {
-		navigator.clipboard.writeText(window.location);
-		alert('Copied url!');
-		return 0;
-	}
+  function handleClick() {
+    navigator.clipboard.writeText(window.location);
+    alert("Copied url!");
+    return 0;
+  }
 </script>
 
 <div class={styles.mainContainer}>
-	<a href={ghLink} target="_blank" rel="noreferrer">
-		<button class={styles.button}>
-			<img src="src\static\logo\githublogo.png" alt="github" class={styles.icon} />
-		</button>
-	</a>
-	<a href={liLink} target="_blank" rel="noreferrer"
-		><button class={styles.button}>
-			<img src="src/static/logo/linkedinlogo.png" alt="linkedin" class={styles.icon} />
-		</button>
-	</a>
+  <a href={ghLink} target="_blank" rel="noreferrer">
+    <button class={styles.button}>
+      <img src="logo\githublogo.png" alt="github" class={styles.icon} />
+    </button>
+  </a>
+  <a href={liLink} target="_blank" rel="noreferrer"
+    ><button class={styles.button}>
+      <img src="logo/linkedinlogo.png" alt="linkedin" class={styles.icon} />
+    </button>
+  </a>
 
-	<button class={styles.button} on:click={() => handleClick()}>
-		<img src="src/static/logo/copy.svg" alt="copy" class={styles.icon} />
-	</button>
+  <button class={styles.button} on:click={() => handleClick()}>
+    <img src="logo/copy.svg" alt="copy" class={styles.icon} />
+  </button>
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,35 +1,39 @@
 <script>
-	//svelte
+  //svelte
 
-	//styles
-	import styles from './styles.module.css';
+  //styles
+  import styles from "./styles.module.css";
 
-	//components
-	import DownArrowButton from '../components/downArrowButton/downArrowButton.svelte';
-	import LinkBar from '../components/linkBar/linkBar.svelte';
+  //components
+  import DownArrowButton from "../components/downArrowButton/downArrowButton.svelte";
+  import LinkBar from "../components/linkBar/linkBar.svelte";
 </script>
 
 <div class={styles.appContainer}>
-	<div class={styles.homeBg} />
-	<div class={styles.infoContainer}>
-		<div class={styles.pfpContainer}>
-			<img src="src/static/pfp/pfp.png" alt="pfp" class={styles.pfp} />
-			<div class={styles.nameContainer}>
-				<h1>Hello!</h1>
-				<h1>I'm Lenny Xiong</h1>
-			</div>
-		</div>
-		<h2>I code things for the web!!</h2>
-		<LinkBar
-			ghLink="https://github.com/Rurucchi"
-			liLink="https://www.linkedin.com/in/lenny-xiong/"
-		/>
-	</div>
-	<DownArrowButton />
+  <div class={styles.homeBg} />
+  <div class={styles.infoContainer}>
+    <div class={styles.pfpContainer}>
+      <img src="pfp/pfp.png" alt="pfp" class={styles.pfp} />
+      <div class={styles.nameContainer}>
+        <h1>Hello!</h1>
+        <h1>I'm Lenny Xiong</h1>
+      </div>
+    </div>
+    <h2>I code things for the web!!</h2>
+    <LinkBar
+      ghLink="https://github.com/Rurucchi"
+      liLink="https://www.linkedin.com/in/lenny-xiong/"
+    />
+  </div>
+  <DownArrowButton />
 </div>
 
 <style>
-	h1 {
-		margin: 0;
-	}
+  h1 {
+    margin: 0;
+    font-style: normal;
+  }
+  h2 {
+    font-style: normal;
+  }
 </style>
