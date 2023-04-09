4f1effbd622f12813009b9352cc7ef5c107175a4
Rurucchi <rurucchi.pro@gmail.com>
2023-04-09 14:20:10 +0200

    changed file structure again, trying to fix production

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -12,7 +12,7 @@
         <ProjectBubble
           title="Next Up (6 Month Internship)"
           description="Management app made with React and Express for Highschool and University sports teams"
-          background="src/static/projectImage/nextup.png"
+          background="projectImage/nextup.png"
           link="https://github.com/Next-Up-Technologies"
         />
       </div>
@@ -24,19 +24,19 @@
           title="Read.IT"
           description="Full stack Reddit-like application made with React, Express and MongoDB."
           link="https://github.com/Rurucchi/Read.It"
-          background="src/static/projectImage/readit.png"
+          background="projectImage/readit.png"
         />
         <ProjectBubble
           title="Portfolio"
           description="Portfolio website made with SvelteKit."
           link="https://github.com/Rurucchi/newPortfolio"
-          background="src/static/projectImage/portfolio.png"
+          background="projectImage/portfolio.png"
         />
         <ProjectBubble
           title="tehcheightcalc"
           description="Mini-project made with Winforms (C#) for entertainment"
           link="https://github.com/Rurucchi/tehcheightcalc"
-          background="src/static/projectImage/heightcalc.png"
+          background="projectImage/heightcalc.png"
         />
       </div>
     </div>
